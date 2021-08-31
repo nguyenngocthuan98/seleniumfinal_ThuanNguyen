7c91bc5764a01553c9b962073c8a38fb55c354ab
nguyenngocthuan98 <37949712+nguyenngocthuan98@users.noreply.github.com>
2021-08-31 10:30:46 +0700

    tc03-User can not login with invalid password

--- src/test/java/tests/TC03_LoginInvalidPasswordTest.java
+++ src/test/java/tests/TC03_LoginInvalidPasswordTest.java
@@ -7,28 +7,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
-public class TC02_LoginBlankUsername extends BaseTests {
+public class TC03_LoginInvalidPasswordTest extends BaseTests{
     private final LoginPage loginPage = new LoginPage();
     private final Account account = new Account();
 
-    @Test(testName = "User can't login with blank Username text box")
-    public void tc02_LoginBlankUsernameTest() {
+    @Test(testName = "User can not log into Railway with invalid password")
+    public void tc03_LoginInvalidPasswordTest() {
         LogHelper.logInfo("Go to Login page");
         loginPage.goToLoginPage();
 
-        LogHelper.logInfo("Set Username is blank and valid Password value for Account object");
-        account.setEmail("");
-        account.setPassword(Constants.PASSWORD);
+        LogHelper.logInfo("Set Username value and invalid Password value for Account object");
+        account.setEmail(Constants.EMAIL);
+        account.setPassword("this_is_wrong_password");
 
         LogHelper.logInfo("Login with valid account");
         loginPage.login(account);
 
-        LogHelper.logInfo("Check username error message");
-        Assert.assertEquals(loginPage.getEmailErrorMessage(), "You must specify a username.",
-                "Username error message doesn't display as expected.");
         LogHelper.logInfo("Check login form error message");
         Assert.assertEquals(loginPage.getLoginFormMessage(),
-                "There was a problem with your login and/or errors exist in your form.", 
+                "There was a problem with your login and/or errors exist in your form.",
                 "Login form error message doesn't display as expected.");
     }
 }
