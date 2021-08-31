ed8ca848b1864e4ee68b6f72b5ad46b7d7a4efe8
nguyenngocthuan98 <37949712+nguyenngocthuan98@users.noreply.github.com>
2021-08-31 16:05:53 +0700

    tc04-fix comment 02

--- src/test/java/tests/BookTicketTests.java
+++ src/test/java/tests/BookTicketTests.java
@@ -3,15 +3,17 @@ package tests;
 import helpers.LogHelper;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.BookTicketPage;
 import pages.LoginPage;
 
-public class TC04_BookTicketWithUnloggedUserTest extends BaseTests {
+public class BookTicketTests extends BaseTests {
     private final LoginPage loginPage = new LoginPage();
+    private final BookTicketPage bookTicketPage = new BookTicketPage();
 
     @Test(testName = "Login page displays when un-logged User clicks on Book ticket tab")
     public void tc04_BookTicketWithUnloggedUserTest() {
         LogHelper.logInfo("Go to Book Ticket page");
-        loginPage.goToBookTicketPage();
+        bookTicketPage.goToBookTicketPage();
 
         LogHelper.logInfo("Check login page name");
         Assert.assertEquals(loginPage.getPageNameLabel(), "Login Page",
